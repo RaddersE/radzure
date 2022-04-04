c87a2460d9a9805918f71bfcae7ff4853c999051
Ean Radcliffe <Guru@Tuliam.onmicrosoft.com>
2022-04-04 16:16:46 +0100

    Added RUM component

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+<script type="text/javascript">
+	(function(w,d,s,r,k,h,m){
+	if(w.performance && w.performance.timing && w.performance.navigation) {
+		w[r] = w[r] || function () { (w[r].q = w[r].q || []).push(arguments) };
+	h=d.createElement('script');h.async=true;h.setAttribute('src',s+k);
+	d.getElementsByTagName('head')[0].appendChild(h);
+	(m = window.onerror),(window.onerror = function (b, c, d, f, g) {
+		m && m(b, c, d, f, g), g || (g = new Error(b)), (w[r].q = w[r].q || []).push(["captureException", g]);})
+	}
+})(window,document,'//static.site24x7rum.eu/beacon/site24x7rum-min.js?appKey=','s247r','bfff4dc41b0cfd7862bb4bc64240bfa8');
+</script>
